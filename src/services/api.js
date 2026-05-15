eb83d906d6ee043ba27f8b3372829efa091328ea
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2026-05-15 15:19:02 -0400

    feat: add nginx configuration and update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9090/api/v1',
+  baseURL: 'http://98.90.87.240:9090/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
